43cb8142181d8e7f3b47f43d0f4f97177da82ddc
Leonardo Gregori <leogregori-tech@gmail.com>
2026-06-24 08:47:37 -0300

    feat: complete rectangle OOP exercise with area, perimeter and diagonal logic

---
+++ OOP_Exercise1/OOP_Exercise1/Program.cs
@@ -0,0 +1,9 @@
+﻿Rectangle r = new Rectangle();
+
+Console.WriteLine("Insert rectangle's width and height:");
+r.Width = double.Parse(Console.ReadLine());
+r.Height = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"AREA = {r.areaCalc():F2}");
+Console.WriteLine($"PERIMETER = {r.perimeterCalc():F2}");
+Console.WriteLine($"DIAGONAL = {r.diagonalCalc():F2}");
---
+++ OOP_Exercise1/OOP_Exercise1/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿internal class Rectangle
+{
+    public double Width;
+    public double Height;
+
+    public double areaCalc()
+    {
+        return Width * Height;
+    }
+
+    public double perimeterCalc()
+    {
+        return (Width * 2) + (Height * 2);
+    }
+
+    public double diagonalCalc()
+    {
+        return Math.Sqrt((Math.Pow(Width, 2) + (Math.Pow(Height, 2))));
+    }
+}
