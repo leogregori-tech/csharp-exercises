6859302056c5960ef428c2e539b5817348b0ee3a
Leonardo Gregori <leogregori-tech@gmail.com>
2026-06-24 10:37:25 -0300

    feat: complete employee OOP exercise with net salary and percentage increase method

---
+++ OOP_Exercise2/OOP_Exercise2/Employee.cs
@@ -0,0 +1,21 @@
+﻿internal class Employee
+{
+    public string Name;
+    public double GrossSalary;
+    public double TaxValue;
+
+    public double NetSalary()
+    {
+        return GrossSalary - TaxValue;
+    }
+
+    public void IncreaseSalary(double percentage)
+    {
+        GrossSalary *= (1 + (percentage / 100)) ;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, $ {NetSalary():F2}";
+    }
+}
---
+++ OOP_Exercise2/OOP_Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿Employee e = new Employee();
+
+Console.Write("Name: ");
+e.Name = Console.ReadLine();
+Console.Write("Gross salary: ");
+e.GrossSalary = double.Parse(Console.ReadLine());
+Console.Write("Tax value: ");
+e.TaxValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"Funcionário: {e}");
+
+Console.WriteLine();
+Console.Write("Insert an percentage to increase the salary: ");
+double percentage = double.Parse(Console.ReadLine());
+e.IncreaseSalary(percentage);
+
+Console.WriteLine();
+Console.WriteLine($"Updated data: {e}");
