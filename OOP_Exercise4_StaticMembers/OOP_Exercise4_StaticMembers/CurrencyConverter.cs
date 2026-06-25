ea5644d360fb8ea5eee07da81fa86f6b31b33bb7
Leonardo Gregori <leogregori-tech@gmail.com>
2026-06-25 20:34:00 -0300

    feat: complete static members currency converter POO exercise with USD to Real currency method

---
+++ OOP_Exercise4_StaticMembers/OOP_Exercise4_StaticMembers/CurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿internal class CurrencyConverter
+{
+    public static double Iof = 6.0;
+
+    public static double RealToUsd(double qtyUsd, double usdExchangeRate)
+    {
+        return (usdExchangeRate * qtyUsd) * (1 + Iof / 100);
+    }
+}
+
---
+++ OOP_Exercise4_StaticMembers/OOP_Exercise4_StaticMembers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("How much is the USD exchange rate? ");
+double UsdExchangeRate = double.Parse(Console.ReadLine());
+
+Console.Write("How much USD you want to buy? ");
+double qtyUSD = double.Parse(Console.ReadLine());
+
+double realToUsd = CurrencyConverter.RealToUsd(UsdExchangeRate, qtyUSD);
+Console.WriteLine($"Value to be paid in R$: {realToUsd:F2}");
+
