fef6df74c23e061c28c2a95ca4e202d3b7882dee
Leonardo Gregori <leogregori-tech@gmail.com>
2026-06-30 11:47:32 -0300

    feat: complete BankAccount OOP exercise with constructors, 'this' word, overloading and encapsuling

---
+++ OOP_Exercise5/OOP_Exercise5/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿internal class BankAccount
+{
+    public int AccountNumber { get; private set; }
+    public string HolderName { get; set; }
+    public double Balance { get; private set; }
+
+    public BankAccount(int accountNumber, string holderName) {
+        AccountNumber = accountNumber;
+        HolderName = holderName;
+    }
+
+    public BankAccount(int accoutNumber, string holderName, double valueInitialDeposit) : this (accoutNumber, holderName)
+    {
+        Deposit(valueInitialDeposit);
+    }
+
+    public void Deposit(double depositValue)
+    {
+        Balance += depositValue;
+    }
+
+    public void Withdrawal(double withdrawalValue)
+    {
+        double withdrawalTax = 5.0;
+        Balance -= withdrawalValue;
+        Balance -= withdrawalTax;
+    }
+
+    public override string ToString()
+    {
+        return $"Account {AccountNumber}, Holder: {HolderName}, Balance: $ {Balance:F2}";
+    }
+}
---
+++ OOP_Exercise5/OOP_Exercise5/Program.cs
@@ -0,0 +1,37 @@
+﻿BankAccount account;
+
+Console.Write("Insert the account number: ");
+int accountNumber = int.Parse(Console.ReadLine());
+Console.Write("Insert the bank account holder: ");
+string holderName = Console.ReadLine();
+Console.Write("It will be made an initial deposit (y/n)? ");
+char answerInitialDeposit = char.Parse(Console.ReadLine());
+
+if (answerInitialDeposit == 'y' || answerInitialDeposit == 'Y')
+{
+    Console.Write("Insert the initial deposit value: ");
+    double valueInitialDeposit = double.Parse(Console.ReadLine());   
+    account = new BankAccount(accountNumber, holderName, valueInitialDeposit)
+}
+else
+{
+    account = new BankAccount(accountNumber, holderName);
+}
+
+Console.WriteLine();
+Console.WriteLine("Account data:");
+Console.WriteLine(account);
+
+Console.WriteLine();
+Console.Write("Insert a value to be deposited: ");
+double value = double.Parse(Console.ReadLine());
+account.Deposit(value);
+Console.WriteLine("Updated account data:");
+Console.WriteLine(account);
+
+Console.WriteLine();
+Console.Write("Insert a value to be withdrawn: ");
+value = double.Parse(Console.ReadLine());
+account.Withdrawal(value);
+Console.WriteLine("Updated account data:");
+Console.WriteLine(account);
